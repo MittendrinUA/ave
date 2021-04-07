ef664f6cecc9ccbf291af24699e69a62a0bbffb5
MittendrinUA <kondratykv@gmail.com>
2021-04-07 19:12:02 +0300

    Lookbook ready. Sign ready.
    1. Lookbook ready
    2. Sign ready
    3. Small fixes:
    - пофіксив .button (Тепер ширина блока визначається автоматично)
    - дав .container @media(max-width:610px)
    - інші маленькі фікси адаптивки та іншого
    =)

---
+++ Lookbook/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.header_burger').click(function(event) {
+        $('.header_burger,.mainHeader--nav,.mainHeader--logo,.mainHeader--top').toggleClass('active');
+    });
+});
